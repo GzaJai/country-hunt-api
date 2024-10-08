61d0207874bc3089529cc9667e6136cdb35d4354
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-10-08 15:24:45 -0300

    testing fastapi example

--- countries-api/main.py
+++ countries-api/main.py
@@ -1,32 +1,14 @@
 from typing import Union
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from handlers import get_response
 
 app = FastAPI()
 
-# Set up CORS
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://localhost:5173",
-    "https://*.vercel.app",
-    # Add more origins as needed
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,  # Allows all origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
-)
 
 @app.get("/")
 def read_root():
-    return get_response()
+    return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
