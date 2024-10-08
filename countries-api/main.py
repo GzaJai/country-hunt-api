97ab2d2fd14401705bc5445066001cc4415dc04d
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-10-08 15:56:27 -0300

    deleting CORS config

--- countries-api/main.py
+++ countries-api/main.py
@@ -1,23 +1,9 @@
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from handlers import get_response
 from typing import Union
 
 app = FastAPI()
 
-# Set up CORS to allow all origins
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allows all headers (Authorization, Content-Type, etc.)
-)
-
 @app.get("/")
 def read_root():
     return get_response()
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
