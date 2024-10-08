b0634cc74108c08e8e493b6856363ffcde60115f
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-10-08 15:27:03 -0300

    testing with API handlers

--- countries-api/main.py
+++ countries-api/main.py
@@ -2,13 +2,13 @@ from typing import Union
 
 from fastapi import FastAPI
 
-app = FastAPI()
+from handlers import get_response
 
+app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
+    return get_response()
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
